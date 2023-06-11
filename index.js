6af18b615f570dfa803d46b187f5ff60b269db47
Jose Herrera <jdj.herrera27@gmail.com>
2023-06-11 09:17:12 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+
+const square = (parameter3) => parameter3 * parameter3;
+
+const add = (parameter4, parameter5) => parameter4 + parameter5;
